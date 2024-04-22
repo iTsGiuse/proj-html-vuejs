58ed88163e6c2538c228e4e62bc11b3a68e8b3de
iTsGiuse <giuse.amato@icloud.com>
2024-04-22 12:45:44 +0200

    new project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+  import AppHeader from './components/Header/AppHeader.vue';
+
+  export default{
+    components: {
+      AppHeader,
+    }
+  }
+
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss">
+
+/*   @use './style/generic' as *;
+  @use './style/partials/variables' as *; */
+
+</style>
